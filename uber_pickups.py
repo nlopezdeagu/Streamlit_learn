f46c17995ae690e9de05a5a35bb046df38b24e66
Nicolas <nlopezdeagu@unav.es>
2025-05-16 19:30:34 +0200

    Esto es una prueba de un commit

---
+++ uber_pickups.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
+st.markdown('This app visualizes Uber pickups in NYC')
+st.sidebar.markdown('## Filter data')
